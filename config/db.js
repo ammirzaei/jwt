14490702e810509550c00fe1d307305842207da4
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-02-24 22:54:06 +0330

    feat: connect to mongodb

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 module.exports = async ()=>{
     try {
-        
+        mongoose.set('strictQuery', true);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`Mongodb Connected on ${conn.connection.host}`);
     } catch (err) {
         console.log(err);
         process.exit(1);
--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const dotEnv = require('dotenv');
 
 const app = new express();
+const mongoConnect = require('./config/db');
 
 // config dotenv 
-dotEnv.config({ path : './config/config.env'});
+dotEnv.config({ path: './config/config.env' });
+
+// connected to mongodb
+mongoConnect();
 
 
-app.listen(process.env.PORT,()=>{
+// running project
+app.listen(process.env.PORT, () => {
     console.log(`Server is Running on port ${process.env.PORT}`);
 });
